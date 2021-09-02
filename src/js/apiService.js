8ac63845c4a840f5c2662fac0ee027d84d8645db
Viktor Mostovyi <mvvfreelance@gmai.com>
2021-09-02 18:29:13 +0300

    add api

---
+++ src/js/apiService.js
@@ -0,0 +1,35 @@
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '21680685-e17dbc926fcd99c5b67998b00';
+
+export default class ImagesApiService {
+  constructor() {
+    this.searchQuery = 'cat';
+    this.page = 1;
+  }
+
+  fetchImages() {
+    const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
+    return fetch(url)
+      .then(response => response.json())
+      .then(({ hits }) => {
+        this.incrementPage();
+        return hits;
+      });
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
---
+++ src/js/getRefs.js
@@ -0,0 +1,7 @@
+export default function getRefs() {
+  return {
+    form: document.querySelector('.js-search-form'),
+    gallery: document.querySelector('.js-gallery'),
+    btn: document.querySelector('.js-btn'),
+  };
+}
